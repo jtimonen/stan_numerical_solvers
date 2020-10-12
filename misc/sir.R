7f243f3faadc42a2787b3548a3271ff5064272a5
jtimonen <juho.timonen@iki.fi>
2020-10-12 16:06:55 +0300

    polish a bit

--- misc/sir.R
+++ misc/sir.R
@@ -1,4 +1,10 @@
+
+# https://mc-stan.org/users/documentation/case-studies/boarding_school_case_study.html
+
 require(rstan)
 
 model <- rstan::stan_model(file = 'sir.stan')
 expose_stan_functions(model)
+
+N <- 100
+f <- stan_sir(0.0, c(1,2,3), c(1,1), 0.0, 10.0)
