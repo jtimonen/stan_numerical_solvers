dff9a3aea0320fb9a39acf51d49d836b112e9eec
jtimonen <juho.timonen@iki.fi>
2021-02-28 14:59:59 +0200

    more R files and organization of ode case study

---
+++ case_study_ode/R/sir.R
@@ -0,0 +1,43 @@
+library(cmdstanr)
+library(posterior)
+library(loo)
+source("functions.R")
+
+# Compile model
+model <- cmdstan_model("stan/lv.stan", include_paths = "stan")
+model_sim <- cmdstan_model("stan/lv_sim.stan", include_paths = "stan")
+
+# Load test data
+dat <- readRDS(file = "data_small.rds")
+
+# Fit model
+h <- 1.0
+tol <- 1e-6
+fit <- sample_lv(model, dat, h, tol = tol,
+  solver = 0, refresh = 1000, chains = 10, init = 0
+)
+
+# Plot solution with posterior mean params
+theta_mean <- apply(fit$draws(variables = "theta"), 3, mean)
+t_eval <- seq(0.1, 12, by = 0.1)
+out <- solve_lv(model_sim, theta_mean, t_eval, h)
+plot_lv(dat, out)
+diag <- fit$cmdstan_diagnose()
+
+# Comparison
+fit_ref <- sample_lv(model, dat, h, tol = tol,
+  solver = 1, refresh = 1000, chains = 10, init = 0
+)
+
+# Print info
+runtime_info(fit)
+runtime_info(fit_ref)
+
+# Get log ratios, compute r_eff and pareto_k
+#log_ratios <- fit$draws(variables = "log_ratio")
+#r_eff <- as.numeric(relative_eff(log_ratios))
+#psis <- psis(log_ratios, r_eff = r_eff)
+
+#msg3 <- paste0("Pareto_k = ", round(psis$diagnostics$pareto_k, 3), "\n")
+#cat(msg3)
+
---
+++ case_study_ode/R/sir_sim.R
@@ -0,0 +1,27 @@
+library(cmdstanr)
+source("functions.R")
+
+# Compile model
+model_sim <- cmdstan_model("stan/lv_sim.stan", include_paths = "stan")
+
+# Simulate data
+N <- 30
+D <- 2
+theta <- c(1.2, 0.8)
+t_data <- seq(12/N, 12, length.out = N) + 0.3 * rnorm(n = length(N))
+if (min(t_data) < 0) {
+  t_data <- sort(t_data) - min(t_data) + 0.01
+}
+h <- 1.0
+out <- solve_lv(model_sim, theta, t_data, h)
+
+# Add noise
+y_data <- out$y_ref
+y_data <- y_data + matrix(0.3 * rnorm(n = D * N), N, D)
+dat <- list(t_data = t_data, y_data = y_data, N = N, D = D)
+
+# Plot
+plot_lv(dat, out)
+
+# Write to file
+saveRDS(file = "data_small.rds", dat)
