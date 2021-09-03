ff8dba622da7e6b0b135af539c6f1ecc81a3460e
jtimonen <juho.timonen@iki.fi>
2021-09-04 00:22:19 +0300

    improve plots

--- case_study_ode/R/odeadapt.R
+++ case_study_ode/R/odeadapt.R
@@ -74,64 +74,75 @@ simulate <- function(model, params, data, solver_args = list()) {
   stopifnot(is(params, "draws"))
   stopifnot(is(data, "list"))
   stopifnot(is(solver_args, "list"))
-  if(is.null(solver_args$RTOL)) solver_args$RTOL <- 1e-6
-  if(is.null(solver_args$ATOL)) solver_args$ATOL <- 1e-6
-  if(is.null(solver_args$MAX_NUM_STEPS)) solver_args$MAX_NUM_STEPS <- 1e6
+  if (is.null(solver_args$RTOL)) solver_args$RTOL <- 1e-6
+  if (is.null(solver_args$ATOL)) solver_args$ATOL <- 1e-6
+  if (is.null(solver_args$MAX_NUM_STEPS)) solver_args$MAX_NUM_STEPS <- 1e6
   model$generate_quantities(
-    data = c(data, solver_args), 
+    data = c(data, solver_args),
     fitted_params = params
   )
 }
 
 # Using simulate with different tolerances
-simulate_many <- function(model, params, data, 
-                          atols, rtols, MAX_NUM_STEPS=NULL) {
+simulate_many <- function(model, params, data,
+                          atol, rtol, MAX_NUM_STEPS = NULL) {
   stopifnot(is(params, "draws"))
-  J1 <- length(atols)
-  J2 <- length(rtols)
+  J1 <- length(atol)
+  J2 <- length(rtol)
   S <- niterations(params) * nchains(params)
-  TIME <- array(0, dim=c(J1, J2))
-  XSIM <- array(0, dim=c(J1, J2, S, data$N*data$D))
-  for(j1 in 1:J1) {
-    for(j2 in 1:J2) {
+  TIME <- array(0, dim = c(J1, J2))
+  XSIM <- array(0, dim = c(J1, J2, S, data$N * data$D))
+  for (j1 in 1:J1) {
+    for (j2 in 1:J2) {
       solver_args <- list(
-        ATOL = atols[j1], 
-        RTOL=rtols[j2],
+        ATOL = atol[j1],
+        RTOL = rtol[j2],
         MAX_NUM_STEPS = MAX_NUM_STEPS
       )
       sim <- simulate(model, params, data, solver_args)
-      XSIM[j1, j2,,]  <- merge_chains(sim$draws("x"))[,1,, drop=TRUE]
+      XSIM[j1, j2, , ] <- merge_chains(sim$draws("x"))[, 1, , drop = TRUE]
       TIME[j1, j2] <- sim$time()$total
     }
   }
-  return(list(times=TIME,sims=XSIM))
+  return(list(times = TIME, sims = XSIM))
 }
 
 # Compute error to most accurate solution
-compute_errors <- function(XSIM, fun="max") {
+compute_errors <- function(XSIM, fun = "max") {
   J1 <- dim(XSIM)[1]
   J2 <- dim(XSIM)[2]
-  ERR <- array(0, dim=c(J1,J2))
-  I_sim_best <- XSIM[1,1,,]
-  for(j1 in 1:J1) {
-    for(j2 in 1:J2) {
-      I_sim <- XSIM[j1,j2,,]
-      abs_errors <- abs(as.vector(I_sim_best)-as.vector(I_sim))
-      ERR[j1,j2] <- eval(call(fun, abs_errors))
+  ERR <- array(0, dim = c(J1, J2))
+  I_sim_best <- XSIM[1, 1, , ]
+  for (j1 in 1:J1) {
+    for (j2 in 1:J2) {
+      I_sim <- XSIM[j1, j2, , ]
+      abs_errors <- abs(as.vector(I_sim_best) - as.vector(I_sim))
+      ERR[j1, j2] <- eval(call(fun, abs_errors))
     }
   }
   return(ERR)
 }
 
 # Runtimes plot
-plot_sim_times <- function(atols, rtols, TIME) {
-  par(mfrow=c(1,2))
-  image(log10(atols), log10(rtols), TIME, main = "time (s)")
+plot_sim_times <- function(atol, rtol, TIME) {
+  par(mfrow = c(1, 2))
+  plot(log10(rtol), diag(TIME),
+    xlab = "log10(tol)", ylab = "time (s)",
+    type = "o", pch = 16
+  )
+  grid()
+  image(log10(atol), log10(rtol), TIME, main = "time (s)")
 }
 
 
 # Errors plot
-plot_sim_errors <- function(atols, rtols, ERR) {
-  par(mfrow=c(1,2))
-  image(log10(atols), log10(rtols), log(ERR), main="log error")
+plot_sim_errors <- function(atol, rtol, ERR) {
+  main <- paste0("log ", deparse(substitute(ERR)))
+  par(mfrow = c(1, 2))
+  plot(log10(rtol), diag(log(ERR)),
+    xlab = "log10(tol)", ylab = main,
+    type = "o", pch = 16
+  )
+  grid()
+  image(log10(atol), log10(rtol), log(ERR), main = main)
 }
