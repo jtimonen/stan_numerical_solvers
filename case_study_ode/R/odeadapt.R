88765a5e9d2e557e5cd35f8fa974eeeb5b2abda7
jtimonen <juho.timonen@iki.fi>
2021-09-04 01:45:37 +0300

    add loglik error computation and plots

--- case_study_ode/R/odeadapt.R
+++ case_study_ode/R/odeadapt.R
@@ -15,33 +15,31 @@ stan_code <- function(blocks, codes) {
   return(model_code)
 }
 
-# Create a CmdStanModel for prior sampling
-create_prior_model <- function(pars, tpars, prior) {
+# Create full Stan code for prior sampling model
+prior_model_code <- function(pars, tpars, prior) {
   cat("* Creating CmdStanModel for prior sampling...\n")
   blocks <- c("parameters", "transformed parameters", "model")
   codes <- c(pars, tpars, prior)
-  model_code <- stan_code(blocks, codes)
-  tmp_file <- cmdstanr::write_stan_file(model_code)
-  cmdstanr::cmdstan_model(tmp_file)
+  stan_code(blocks, codes)
 }
 
-# Create a CmdStanModel for simulating data given param draws
-create_simulator_model <- function(funs, data, tdata, pars, tpars, ode, gq) {
+# Create full Stan code for simulator model
+simulator_model_code <- function(funs, data, tdata, obsdata,
+                                 pars, tpars, ode, gq) {
   cat("* Creating CmdStanModel for simulating...\n")
   blocks <- c(
     "functions", "data", "transformed data",
     "parameters", "transformed parameters", "generated quantities"
   )
+  data <- paste(data, obsdata, sep = "\n")
   gq <- paste(ode, gq, sep = "\n")
   codes <- c(funs, data, tdata, pars, tpars, gq)
-  model_code <- stan_code(blocks, codes)
-  tmp_file <- cmdstanr::write_stan_file(model_code)
-  cmdstanr::cmdstan_model(tmp_file)
+  stan_code(blocks, codes)
 }
 
-# Create a CmdStanModel for posterior sampling
-create_posterior_model <- function(funs, data, tdata, obsdata,
-                                   pars, tpars, prior, ode, lik) {
+# Create full Stan code for posterior sampling model
+posterior_model_code <- function(funs, data, tdata, obsdata,
+                                 pars, tpars, prior, ode, lik) {
   cat("* Creating CmdStanModel for posterior sampling...\n")
   blocks <- c(
     "functions", "data", "transformed data",
@@ -50,22 +48,31 @@ create_posterior_model <- function(funs, data, tdata, obsdata,
   post <- paste(prior, ode, lik, sep = "\n")
   data <- paste(data, obsdata, sep = "\n")
   codes <- c(funs, data, tdata, pars, tpars, post)
-  model_code <- stan_code(blocks, codes)
-  tmp_file <- cmdstanr::write_stan_file(model_code)
-  cmdstanr::cmdstan_model(tmp_file)
+  stan_code(blocks, codes)
 }
 
 # Create all CmdStanModels
 create_cmdstan_models <- function(funs, data, tdata, obsdata, pars,
                                   tpars, prior, ode, lik, gq) {
-  list(
-    prior = create_prior_model(pars, tpars, prior),
-    sim = create_simulator_model(funs, data, tdata, pars, tpars, ode, gq),
-    post = create_posterior_model(
+  codes <- list(
+    prior = prior_model_code(pars, tpars, prior),
+    simulator = simulator_model_code(
+      funs, data, tdata, obsdata,
+      pars, tpars, ode, gq
+    ),
+    posterior = posterior_model_code(
       funs, data, tdata, obsdata,
       pars, tpars, prior, ode, lik
     )
   )
+  j <- 0
+  models <- list()
+  for (code in codes) {
+    j <- j + 1
+    models[[j]] <- cmdstan_model(write_stan_file(code))
+  }
+  names(models) <- names(codes)
+  return(models)
 }
 
 # Function for simulating ODE solutions and data given parameter( draws)s
@@ -92,6 +99,7 @@ simulate_many <- function(model, params, data,
   S <- niterations(params) * nchains(params)
   TIME <- array(0, dim = c(J1, J2))
   XSIM <- array(0, dim = c(J1, J2, S, data$N * data$D))
+  LL <- array(0, dim = c(J1, J2, S))
   for (j1 in 1:J1) {
     for (j2 in 1:J2) {
       solver_args <- list(
@@ -102,13 +110,14 @@ simulate_many <- function(model, params, data,
       sim <- simulate(model, params, data, solver_args)
       XSIM[j1, j2, , ] <- merge_chains(sim$draws("x"))[, 1, , drop = TRUE]
       TIME[j1, j2] <- sim$time()$total
+      LL[j1, j2, ] <- merge_chains(sim$draws("log_lik"))[, 1, 1, drop = TRUE]
     }
   }
-  return(list(times = TIME, sims = XSIM))
+  return(list(times = TIME, sims = XSIM, log_liks = LL))
 }
 
 # Compute error to most accurate solution
-compute_errors <- function(XSIM, fun = "max") {
+compute_sol_errors <- function(XSIM, fun = "max") {
   J1 <- dim(XSIM)[1]
   J2 <- dim(XSIM)[2]
   ERR <- array(0, dim = c(J1, J2))
@@ -117,6 +126,22 @@ compute_errors <- function(XSIM, fun = "max") {
     for (j2 in 1:J2) {
       I_sim <- XSIM[j1, j2, , ]
       abs_errors <- abs(as.vector(I_sim_best) - as.vector(I_sim))
+      ERR[j1, j2] <- eval(call(fun, abs_errors))
+    }
+  }
+  return(ERR)
+}
+
+# Compute error in likelihood, compared to most accurate solution
+compute_loglik_errors <- function(LL, fun = "max") {
+  J1 <- dim(LL)[1]
+  J2 <- dim(LL)[2]
+  ERR <- array(0, dim = c(J1, J2))
+  loglik_best <- LL[1, 1, ]
+  for (j1 in 1:J1) {
+    for (j2 in 1:J2) {
+      loglik <- LL[j1, j2, ]
+      abs_errors <- abs(as.vector(loglik_best) - as.vector(loglik))
       ERR[j1, j2] <- eval(call(fun, abs_errors))
     }
   }
